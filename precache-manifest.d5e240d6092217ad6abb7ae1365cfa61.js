2b35e5dbc60c0882a336e5baf0bb85ccdc8cb2d0
btuchowski <btuchowski@fream.pl>
2019-01-29 08:52:58 +0100

    Updates

---
+++ precache-manifest.d5e240d6092217ad6abb7ae1365cfa61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbc0b85930a36f545c5858d43e6bf423",
+    "url": "/pomodoro_clock/static/media/alert.cbc0b859.mp3"
+  },
+  {
+    "revision": "7557d0226ffe5d22100b",
+    "url": "/pomodoro_clock/static/js/runtime~main.7557d022.js"
+  },
+  {
+    "revision": "78a63066afb3575cb567",
+    "url": "/pomodoro_clock/static/js/main.78a63066.chunk.js"
+  },
+  {
+    "revision": "a48b93a5bebc1e3fffe6",
+    "url": "/pomodoro_clock/static/js/1.a48b93a5.chunk.js"
+  },
+  {
+    "revision": "78a63066afb3575cb567",
+    "url": "/pomodoro_clock/static/css/main.7b684bdf.chunk.css"
+  },
+  {
+    "revision": "abeb6dbf0bccc3785e6c0f81097c6d69",
+    "url": "/pomodoro_clock/index.html"
+  }
+];
